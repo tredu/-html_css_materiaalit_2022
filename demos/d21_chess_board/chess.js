0ff3258f64aa38c7e7e6e8cd6db28dfc1addbe9b
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2022-12-08 15:28:01 +0200

    add e21

---
+++ demos/d21_chess_board/chess.js
@@ -0,0 +1,59 @@
+// w3schools: HTML Drag and Drop API
+// Edit: Aki Kurvinen
+
+function allowDrop(ev) {
+   ev.preventDefault();
+}
+
+function drag(ev) {
+   ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+   ev.preventDefault();
+
+   const data = ev.dataTransfer.getData("text");
+   const moving_id = parseInt(data.replace('drag', ''));
+   let target_id = ev.target.getAttribute('id')
+
+   if (target_id.includes('square')){
+      // landed on empty square
+      ev.target.innerHTML = ''
+      ev.target.appendChild(document.getElementById(data));
+      return;
+   }
+
+   target_id = parseInt(target_id.replace('drag', ''));
+
+   if (target_id > 15 && moving_id > 15){
+      // cancel move if landed on own
+      return
+   }
+   else if (target_id <= 15 && moving_id <= 15){
+      // cancel move if landed on own
+      return
+   }
+   else{
+      // landed on opponent
+      ev.target.innerHTML = ''
+      ev.target.appendChild(document.getElementById(data));
+   }
+
+}
+
+// chess setup
+function init(){
+    const pieces = document.querySelectorAll('span');
+    pieces.forEach((element, index)=> {
+        element.setAttribute('id', `drag${index}`)
+        element.setAttribute('draggable', "true")
+        element.setAttribute('ondragstart', "drag(event)")
+    });
+
+    const squares = document.querySelectorAll('table td');
+    squares.forEach((element, index)=> {
+        element.setAttribute('id', `square${index}`)
+        element.setAttribute('ondrop', "drop(event)")
+        element.setAttribute('ondragover', "allowDrop(event)")
+    });
+}
